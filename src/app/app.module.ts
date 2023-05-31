5d893c4ef4c7baad90165ffbf255c3e2e3ce44f4
jorge castillo <jcastillo@consejodeauditoria.gob.cl>
2023-05-31 12:17:24 -0400

    fin seccion 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 // import { HeroComponent } from './heroes/hero/hero.component';
 // import { ListComponent } from './heroes/list/list.component';
 import { HeroesModule } from './heroes/heroes.module';
-
+import { DbzModule } from './dbz/dbz.module';
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +13,8 @@ import { HeroesModule } from './heroes/heroes.module';
   imports: [
     BrowserModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
